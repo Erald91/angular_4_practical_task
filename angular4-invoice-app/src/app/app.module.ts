16821a8e673d5945a3776a3060b22f4e7549e970
eraldcafka <erald.c@scopicsoftware.com>
2018-05-15 19:30:21 +0200

    + Added HTML template for Login Form and basic styling + Added initial attributes to input fields in order to trigger NgForm directive correctly

--- angular4-invoice-app/src/app/app.module.ts
+++ angular4-invoice-app/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Route } from '@angular/router';
 
 import { GuardedAppModule } from './guarded-app/guarded-app.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-const routesList: Array<Route> = [];
+const routesList: Array<Route> = [
+  { path:'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
+    LoginComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     GuardedAppModule,
     RouterModule.forRoot(routesList)
   ],
